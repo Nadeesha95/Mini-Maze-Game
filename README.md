d82fb9fb08f345e71cb35d72aa993acfb65f7415
Nadeesha Weerasekara <nadeesha@unionassurance.com>
2023-11-02 16:30:41 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Mini-Maze-Game
-"Mini Maze Game" is a fun and challenging pygame that tests your skills in navigating a maze while avoiding moving obstacles.
+<h1 class="code-line" data-line-start=0 data-line-end=1 ><a id="Mini_Maze_Game_0"></a>Mini Maze Game</h1>
+<p class="has-line-data" data-line-start="3" data-line-end="4">“Mini Maze Game” is a simple yet entertaining pygame that puts your navigation and avoidance skills to the test. Navigate a maze with your car, avoiding fast-moving lines that could lead to collisions. With multiple levels and colorful surprises, this game offers an engaging gaming experience.</p>
+<h2 class="code-line" data-line-start=5 data-line-end=6 ><a id="Features_5"></a>Features</h2>
+<ul>
+<li class="has-line-data" data-line-start="7" data-line-end="8">Multiple challenging levels.Multiple challenging levels.</li>
+<li class="has-line-data" data-line-start="8" data-line-end="9">Dynamic and colorful graphics.</li>
+<li class="has-line-data" data-line-start="9" data-line-end="10">Engaging gameplay that tests your reflexes.</li>
+</ul>
+<h2 class="code-line" data-line-start=14 data-line-end=15 ><a id="Installation_14"></a>Installation</h2>
+<p class="has-line-data" data-line-start="16" data-line-end="19">Dillinger requires<br>
+Python 3.x<br>
+Pygame library</p>
+<p class="has-line-data" data-line-start="20" data-line-end="21">Clone the repository to your local machine:</p>
+<pre><code class="has-line-data" data-line-start="23" data-line-end="27" class="language-sh">bash
+git <span class="hljs-built_in">clone</span> https://github.com/yourusername/mini-maze-game.git
+
+</code></pre>
+<p class="has-line-data" data-line-start="28" data-line-end="29">Install the Pygame library if you haven’t already:</p>
+<pre><code class="has-line-data" data-line-start="31" data-line-end="35" class="language-sh">bash
+pip install pygame
+
+</code></pre>
+<p class="has-line-data" data-line-start="36" data-line-end="37">Run the game with the following command:</p>
+<pre><code class="has-line-data" data-line-start="39" data-line-end="43" class="language-sh">bash
+python mini_maze_game.py
+
+</code></pre>
+<h2 class="code-line" data-line-start=43 data-line-end=44 ><a id="License_43"></a>License</h2>
+<p class="has-line-data" data-line-start="45" data-line-end="47">Developed By Nadeesha Weerasekara<br>
+**Free Software, **</p>
---
+++ game.py
@@ -0,0 +1,185 @@
+import pygame
+import sys
+import random
+from random import randint
+
+
+pygame.init()
+pygame.mixer.init()
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+CELL_SIZE = 40
+FPS = 60
+WHITE = (255, 255, 255)
+collision_sound = pygame.mixer.Sound("audio/crash-6711.mp3")
+level_sound = pygame.mixer.Sound("audio/level.mp3")
+won_sound = pygame.mixer.Sound("audio/won.mp3")
+
+# Initialize the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mini Maze Game")
+
+def generate_maze():
+   
+    return [[0 for _ in range(WIDTH // CELL_SIZE)] for _ in range(HEIGHT // CELL_SIZE)]
+
+# Initialize the player character 
+player_image = pygame.image.load("img/movingcar.png") 
+player_image = pygame.transform.scale(player_image, (CELL_SIZE, CELL_SIZE))
+player_rect = player_image.get_rect()
+rotation_angle = 0 
+
+
+home_image = pygame.image.load("img/home.png") 
+home_image = pygame.transform.scale(home_image, (CELL_SIZE, CELL_SIZE))
+
+grid = generate_maze()
+
+# Find the start and end positions
+start = (0, 0)
+end = (len(grid[0]) - 1, len(grid) - 1)
+
+# Initialize the moving lines
+lines = []
+line_speed = 0.1
+line_count = 1
+current_level = 0
+
+def generate_lines():
+    return [
+        (random.randint(50, WIDTH), line_speed, random.randint(7, 15), (0, 0, 0))
+        for _ in range(line_count)
+    ]
+
+lines = generate_lines()
+
+#game
+clock = pygame.time.Clock()
+running = True
+won = False
+game_over = False
+font = pygame.font.Font(None, 36)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+
+    if not won and not game_over:
+
+        if keys[pygame.K_LEFT]:
+            rotation_angle = 90  
+            player_rect.move_ip(-5, 0)
+        elif keys[pygame.K_RIGHT]:
+            rotation_angle = -90 
+            player_rect.move_ip(5, 0)
+
+        if keys[pygame.K_UP] and player_rect.top > 0:
+            rotation_angle = 180
+            player_rect.move_ip(0, -5)
+        if keys[pygame.K_DOWN] and player_rect.bottom < HEIGHT:
+            rotation_angle = 180
+            player_rect.move_ip(0, 5)
+
+        rotated_player = pygame.transform.rotate(player_image, rotation_angle)
+        rotated_rect = rotated_player.get_rect(center=player_rect.center)
+        player_rect = rotated_rect
+
+        for i, line_data in enumerate(lines):
+            line_x, line_speed, line_width, _ = line_data
+            line_x += line_speed
+            if line_x > WIDTH:
+                line_x = 0
+                line_speed = random.uniform(0, 3)  
+                line_width = random.randint(5, 10) 
+                lines[i] = (line_x, line_speed, line_width, (0, 0, 0))
+
+            if player_rect.colliderect(pygame.Rect(line_x, random.randint(0, HEIGHT - line_width), line_width, line_width)):
+                game_over = True
+                current_level = 0  # Reset the level to 0
+                collision_sound.play()
+
+   
+    if current_level == 2:
+       
+        
+        random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        screen.fill(random_color)
+        # Add more moving lines at random points
+        additional_lines = generate_lines()
+        lines.extend(additional_lines)
+        line_speed += 1  # Increase line speed after winning level 2
+    else:
+        screen.fill(WHITE)
+
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            cell_rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+
+    screen.blit(rotated_player, player_rect)
+
+    screen.blit(home_image, pygame.Rect(end[0] * CELL_SIZE, end[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+    for line_data in lines:
+        line_x, _, line_width, line_color = line_data
+        pygame.draw.rect(screen, line_color, pygame.Rect(line_x, random.randint(0, HEIGHT - line_width), line_width, line_width))
+
+    if player_rect.colliderect(pygame.Rect(end[0] * CELL_SIZE, end[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE)):
+        won = True
+        level_sound.play()
+        current_level += 1  # Increase the level
+        if current_level >= 2:
+            won_sound.play()
+           
+            random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+            screen.fill(random_color)
+
+            # Add more moving lines at random points
+            additional_lines = generate_lines()
+            lines.extend(additional_lines)
+            text = font.render("You Won", True, (255, 0, 0))
+            text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            screen.blit(text, text_rect)
+            small_font = pygame.font.Font(None, 24)
+            text2 = small_font.render("Developed By Nadeesha", True, (0, 0, 0))
+            text_rect2 = text2.get_rect(bottomleft=(0, HEIGHT))
+            screen.blit(text2, text_rect2)
+            pygame.display.flip()
+            pygame.time.delay(2000)
+            current_level = 0 
+            line_speed = 2  
+        else:
+            text = font.render(f"Level: {current_level}", True, (0, 0, 0))
+            text_rect = text.get_rect(topleft=(10, 10))
+            screen.blit(text, text_rect)
+            pygame.display.flip()
+            pygame.time.delay(2000)  
+        # Reset the game
+        player_rect.topleft = (0, 0)
+        won = False
+        game_over = False
+
+    # Check for game over
+    if game_over:
+        text = font.render("Game Over", True, (255, 0, 0))
+        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(text, text_rect)
+        pygame.display.flip()
+        pygame.time.delay(2000) 
+        # Reset the game
+        player_rect.topleft = (0, 0)
+        won = False
+        game_over = False
+        current_level = 0
+        lines = generate_lines()
+        line_speed = 2  # Reset line speed
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()
